25c87512acbb495471e08f9700e043246ad67c0b
Liezl Puzon <puzon@stanford.edu>
2017-01-30 01:48:08 -0800

    code style

--- components/NotificationModal.js
+++ components/NotificationModal.js
@@ -37,28 +37,29 @@ class NotificationModal extends Component {
               <Card
                 title={this.state.selectedNotif == null? 'Title' : this.state.selectedNotif.title}
                 image={{uri:'http://thedomeproject.net/assets/img/editorials/kiron%20student.jpg'}}>
+
                 <Text style={{marginBottom: 10}}>
                   The idea with React Native Elements is more about component structure than actual design.
                 </Text>
+
                 <Button
                   icon={{name: 'code'}}
                   backgroundColor='#03A9F4'
                   fontFamily='Lato'
                   buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
                   title='VIEW NOW' />
+
                 <TouchableHighlight onPress={() => {
                   this._setModalVisible(!this.state.modalVisible, this.state.selectedNotif)
                 }}>
                   <Text>Hide Modal</Text>
                 </TouchableHighlight>
+
               </Card>
-
-
             </View>
           </View>
 
         </Modal>
-
     );
   }
 }
