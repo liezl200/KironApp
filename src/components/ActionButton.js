cbfdceae2cf35edbfe8d798652afa0f523ecf20a
hendrikniemann <h.neverman@web.de>
2017-03-29 16:22:55 +0200

    Improve coding style in alignment with eslint conf

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -1,28 +1,31 @@
-'use strict';
+import React, { PropTypes } from 'react';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import styles from '../styles';
 
-import React, {Component} from 'react';
-import ReactNative from 'react-native';
-const styles = require('../styles.js');
 const constants = styles.constants;
-const {
-	StyleSheet,
-	Text,
-	View,
-	TouchableOpacity,
-} = ReactNative;
 
-class ActionButton extends Component {
-	render() {
-		return (
-			<View style = {styles.action}>
-				<TouchableOpacity
-					underlayColor = {constants.actionColor}
-					onPress = {this.props.onPress}>
-					<Text style = {styles.actionText}> {this.props.title} </Text>
-				</TouchableOpacity>
-			</View>
-		);
-	}
+const propTypes = {
+  title: PropTypes.string.isRequired,
+  onPress: PropTypes.func.isRequired,
+};
+
+function ActionButton(props) {
+  return (
+    <View style={styles.action}>
+      <TouchableOpacity
+        underlayColor={constants.actionColor}
+        onPress={props.onPress}
+      >
+        <Text style={styles.actionText}> {props.title} </Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+ActionButton.propTypes = propTypes;
 
 module.exports = ActionButton;
