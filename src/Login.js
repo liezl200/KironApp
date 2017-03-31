463347171f2a32949c7c1dcf5521b6113e2066c2
Liezl Puzon <puzon@stanford.edu>
2017-03-31 06:25:24 +0200

    Fix lint errors in src/Login.js

--- src/Login.js
+++ src/Login.js
@@ -4,33 +4,21 @@
 
 import React, { Component } from 'react';
 import {
-  StyleSheet,
   Text,
-  ListView,
   View,
-  TouchableOpacity
 } from 'react-native';
 
-import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
+import { GoogleSignin, GoogleSigninButton } from 'react-native-google-signin';
 import * as firebase from 'firebase';
-
-import {
-  Button
-} from 'react-native-elements';
 
 // Import components
 const App = require('./App');
 const Spinner = require('./components/Spinner');
-const NotificationList = require('./components/NotificationList');
 
-// Import modules
-const firebaseApp = require('./modules/Firebase').firebaseApp;
-const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
-// TODO(liezl): Add OAuth options that will allow a forgot your password thru kiron?
+// TODO: Add OAuth options that will allow a forgot your password
 
-const emailDomain = 'gmail.com';
+const emailDomain = 'kiron.ngo';
 
-// Import styles
 const styles = require('./styles.js');
 
 class Login extends Component {
@@ -41,102 +29,40 @@ class Login extends Component {
       loading: false,
       wrongDomainError: false,
     };
-    //this.usersRef = firebaseApp.database().ref().child('users');
+
+    this.signIn = this.signIn.bind(this);
+    this.signOut = this.signOut.bind(this);
   }
 
   componentDidMount() {
-    this._setupGoogleSignin();
+    this.setupGoogleSignin();
   }
 
-  _renderButton() {
-    if (this.state.loading) {
-      return (<Spinner size='large'/>);
-    }
+  async setupGoogleSignin() {
+    await GoogleSignin.hasPlayServices({ autoResolve: true });
+    await GoogleSignin.configure({
+      iosClientId: '969105153506-3cdeb1bv8d9h1tc8kl7hjvto1gotrmr8.apps.googleusercontent.com',
+      webClientId: '969105153506-d3g2rslivtlkkvdfi34v2h2a7s54n4ir.apps.googleusercontent.com',
+      offlineAccess: false,
+    });
 
-    return (
-      <GoogleSigninButton
-        style={{width: 212, height: 48}}
-        size={GoogleSigninButton.Size.Standard}
-        color={GoogleSigninButton.Color.Light}
-        onPress={this._signIn.bind(this)} />
-    );
+    const user = await GoogleSignin.currentUserAsync();
+    this.setState({ user });
   }
 
-  _renderSignInError() {
-    if (!this.state.loading && (this.state.user
-      && this.state.user.email
-      && !this.state.user.email.endsWith(emailDomain)
-      || this.state.wrongDomainError)) {
-      return (
-        <Text>Please login with your {emailDomain} account</Text>
-      )
-    }
-  }
-  render() {
-
-    if (!this.state.user) {
-      return (
-        <View style={styles.loginContainer}>
-          {this._renderButton()}
-          {this._renderSignInError()}
-        </View>
-      );
-    }
-
-    if (this.state.user) {
-      console.log(this.state.user);
-      return (
-        <App signOut={this._signOut.bind(this)} user={this.state.user}/>
-        // <View style={styles.container}>
-
-        //   <Text style={{fontSize: 18, fontWeight: 'bold', marginBottom: 20}}>Welcome {this.state.user.name}</Text>
-        //   <Text>Your email is: {this.state.user.email}</Text>
-
-        //   <TouchableOpacity onPress={() => {this._signOut(); }}>
-        //     <View style={{marginTop: 50}}>
-        //       <Text>Log out</Text>
-        //     </View>
-        //   </TouchableOpacity>
-        // </View>
-      );
-    }
-  }
-
-  async _setupGoogleSignin() {
-    // try {
-      await GoogleSignin.hasPlayServices({ autoResolve: true });
-      await GoogleSignin.configure({
-        iosClientId: '969105153506-3cdeb1bv8d9h1tc8kl7hjvto1gotrmr8.apps.googleusercontent.com',
-        webClientId: '969105153506-d3g2rslivtlkkvdfi34v2h2a7s54n4ir.apps.googleusercontent.com',
-        offlineAccess: false
-      });
-
-      const user = await GoogleSignin.currentUserAsync();
-      console.log(user);
-      this.setState({user});
-    // }
-    // catch(err) {
-    //   console.log("Google signin error", err.code, err.message);
-    //   throw err;
-    // }
-  }
-
-
-
-  _signIn() {
-    this.setState({loading: true, wrongDomainError: false})
+  signIn() {
+    this.setState({ loading: true, wrongDomainError: false });
     GoogleSignin.signIn()
     .then((user) => {
-      this.setState({loading: false})
-      console.log(user);
+      this.setState({ loading: false });
 
-      var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
-        console.log({'firebase: ': firebaseUser});
+      const unsubscribe = firebase.auth().onAuthStateChanged((firebaseUser) => {
         unsubscribe();
-        var isUserEqual = function (googleUser, firebaseUser) {
-          if (firebaseUser) {
-            var providerData = firebaseUser.providerData;
-            for (var i = 0; i < providerData.length; i++) {
+
+        const isUserEqual = function isUserEqual(googleUser, fbUser) {
+          if (fbUser) {
+            const providerData = fbUser.providerData;
+            for (let i = 0; i < providerData.length; i += 1) {
               if (providerData[i].providerId === firebase.auth.GoogleAuthProvider.PROVIDER_ID &&
                 providerData[i].uid === googleUser.id) {
                 // We don't need to reauth the Firebase connection.
@@ -145,53 +71,79 @@ class Login extends Component {
             }
           }
           return false;
-        }
+        };
 
-        // Check if we are already signed into Firebase with the correct user.
+        // If we are not yet signed into Firebase with the correct user.
         if (!isUserEqual(user, firebaseUser)) {
-          // Prevent registering emails with domains other than kiron.ngo!
+          // Prevent registering emails with other email domains
           if (user.email.endsWith(emailDomain)) {
             // Build Firebase credential with the Google ID token.
-            var credential = firebase.auth.GoogleAuthProvider.credential(user.idToken);
+            const credential = firebase.auth.GoogleAuthProvider.credential(user.idToken);
             // Sign in with the credential from the Google user.
-            console.log(credential);
-            firebase.auth().signInWithCredential(credential).catch(function(error) {
-              console.log(error);
-
-              // TODO: Fancier error handling here / implement domain restriction
-              // var errorCode = error.code;
-              // var errorMessage = error.message;
-              // // The email of the user's account used.
-              // var email = error.email;
-              // // The firebase.auth.AuthCredential type that was used.
-              // var credential = error.credential;
-            });
+            firebase.auth().signInWithCredential(credential);
           }
-        } else {
-          console.log('User already signed-in Firebase.');
         }
       });
+
       if (user && user.email && user.email.endsWith(emailDomain)) {
-        this.setState({user: user});
+        this.setState({ user });
       } else {
-        this.setState({wrongDomainError:true});
-        this._signOut();
+        this.setState({ wrongDomainError: true });
+        this.signOut();
       }
-
-
     })
-    .catch((err) => {
-      console.log('WRONG SIGNIN', err);
-      this.setState({loading: false})
+    .catch(() => {
+      this.setState({ loading: false });
     })
     .done();
   }
 
-  _signOut() {
+  signOut() {
     GoogleSignin.revokeAccess().then(() => GoogleSignin.signOut()).then(() => {
-      this.setState({user: null});
+      this.setState({ user: null });
     })
     .done();
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return (<Spinner size="large" />);
+    }
+
+    return (
+      <GoogleSigninButton
+        style={{ width: 212, height: 48 }}
+        size={GoogleSigninButton.Size.Standard}
+        color={GoogleSigninButton.Color.Light}
+        onPress={this.signIn}
+      />
+    );
+  }
+
+  renderSignInError() {
+    if (!this.state.loading && ((this.state.user
+      && this.state.user.email
+      && !this.state.user.email.endsWith(emailDomain))
+      || this.state.wrongDomainError)) {
+      return (
+        <Text>Please login with your {emailDomain} account</Text>
+      );
+    }
+    return null;
+  }
+
+  render() {
+    if (!this.state.user) {
+      return (
+        <View style={styles.loginContainer}>
+          {this.renderButton()}
+          {this.renderSignInError()}
+        </View>
+      );
+    }
+    return (
+      <App signOut={this.signOut} user={this.state.user} />
+    );
   }
 }
 
